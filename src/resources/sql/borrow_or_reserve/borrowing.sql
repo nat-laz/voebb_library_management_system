f45c8112daafc88f0a2b00762380951b9f4d87c4
Natalie Lazarev <lazarev.n@yahoo.com>
2025-01-22 13:33:32 +0100

    add cancel reservation transaction

--- src/resources/sql/borrow_or_reserve/borrowing.sql
+++ src/resources/sql/borrow_or_reserve/borrowing.sql
@@ -8,3 +8,14 @@ from product_item
          join product on product_item.product_id = product.product_id
          join media_format mf on product.media_format_id = mf.media_format_id
 where item_id = ?;
+
+-- Borrow table more details
+select client_id,
+       borrow.item_id,
+       borrow_start_date,
+       borrow_due_date,
+       borrow_return_date,
+       item_status_name
+from borrow
+         JOIN product_item ON borrow.item_id = product_item.item_id
+         JOIN item_status ON product_item.item_status_id = item_status.item_status_id;
---
+++ src/resources/sql/borrow_or_reserve/cancel_reservation_transaction.sql
@@ -0,0 +1,22 @@
+DO
+$$
+    DECLARE
+        v_item_id INT := ?;
+
+    BEGIN
+        IF EXISTS(SELECT item_id FROM reservation WHERE item_id = v_item_id)
+        THEN
+            -- delete from reservation table
+            DELETE
+            FROM reservation
+            WHERE item_id = v_item_id;
+
+            -- update status to "available"
+            UPDATE product_item
+            SET item_status_id = 1
+            WHERE product_item.item_id = v_item_id;
+        ELSE
+            RAISE EXCEPTION 'Item with id % is not reserved.', v_item_id;
+        END IF;
+    END
+$$;
