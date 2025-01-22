7dfbe14d7eae769c2edc3018c8728508f47eea13
Natalie Lazarev <lazarev.n@yahoo.com>
2025-01-22 16:59:41 +0100

    scheduele cron job for expired reservation

--- src/resources/sql/borrow_or_reserve/cancel_expired_reservation_script.sql
+++ src/resources/sql/borrow_or_reserve/cancel_expired_reservation_script.sql
@@ -1,10 +1,13 @@
-CREATE OR REPLACE FUNCTION expire_reservations_daily()
+CREATE
+    OR REPLACE FUNCTION expire_reservations_daily()
     RETURNS VOID AS
 $$
 DECLARE
     record_reservation RECORD;
 BEGIN
-    FOR record_reservation IN SELECT item_id, reservation_due_date FROM reservation
+    FOR record_reservation IN
+        SELECT item_id, reservation_due_date
+        FROM reservation
         LOOP
             IF (current_date > record_reservation.reservation_due_date) THEN
                 -- re-activate  status to "available"
@@ -13,9 +16,11 @@ BEGIN
                 WHERE product_item.item_id = record_reservation.item_id;
 
                 -- delete record from reservation
-                DELETE FROM reservation
+                DELETE
+                FROM reservation
                 WHERE reservation.item_id = record_reservation.item_id;
             END IF;
         END LOOP;
 END;
-$$ LANGUAGE plpgsql;
+$$
+    LANGUAGE plpgsql;
--- src/resources/sql/borrow_or_reserve/cancel_reservation_transaction.sql
+++ src/resources/sql/borrow_or_reserve/cancel_reservation_transaction.sql
@@ -6,13 +6,17 @@ $$
     BEGIN
         IF EXISTS(SELECT item_id
                   FROM reservation
-                  WHERE item_id = v_item_id
-                     OR (current_date - reservation_start_date) > 3)
+                  WHERE item_id = v_item_id)
         THEN
             -- re-activate  status to "available"
             UPDATE product_item
             SET item_status_id = 1
             WHERE product_item.item_id = v_item_id;
+
+            -- delete record from reservation
+            DELETE
+            FROM reservation
+            WHERE reservation.item_id = v_item_id;
         ELSE
             RAISE EXCEPTION 'Item with id % is not reserved.', v_item_id;
         END IF;
