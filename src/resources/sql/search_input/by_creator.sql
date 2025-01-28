d978943ae492f0ab1b5f9ab54f10430ecfcceae8
bruch-alex <alex.bruch@tutanota.com>
2025-01-28 10:58:37 +0100

    update query

--- src/resources/sql/search_input/by_creator.sql
+++ src/resources/sql/search_input/by_creator.sql
@@ -1,14 +1,14 @@
-SELECT *
-FROM (SELECT avaliable,
-             media_format_name,
-             product_title,
-             product_year,
-             product_photo_link,
-             available_in_libraries,
-             CONCAT(creator.creator_forename || ' ' || creator.creator_lastname) AS creator_full_name
-      FROM main_page_info
-               LEFT JOIN creator_relation ON creator_relation.product_id = main_page_info.product_id
-               LEFT JOIN creator ON creator.creator_id = creator_relation.creator_id
-               LEFT JOIN creator_role ON creator.creator_id = creator_role.role_id) subquery
-WHERE creator_full_name ILIKE '%' || ? || '%'
+SELECT avaliable,
+       media_format_name,
+       product_title,
+       CONCAT(creator.creator_forename || ' ' || creator.creator_lastname) AS creator_full_name,
+       product_year,
+       product_photo_link,
+       available_in_libraries
+FROM main_page_info
+         LEFT JOIN creator_relation ON creator_relation.product_id = main_page_info.product_id
+         LEFT JOIN creator ON creator.creator_id = creator_relation.creator_id
+         LEFT JOIN creator_role ON creator.creator_id = creator_role.role_id
+WHERE creator_forename ILIKE '%' || ? || '%'
+   OR creator_lastname ILIKE '%' || ? || '%'
 LIMIT 22 OFFSET 0;
